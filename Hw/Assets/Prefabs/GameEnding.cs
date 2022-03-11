3b5cd745f9078a9389cdb477ff90f297403572b5
jpay12 <97638950+jpay12@users.noreply.github.com>
2022-03-11 00:08:33 -0500

    attempted to add sound

--- Hw/Assets/Prefabs/GameEnding.cs
+++ Hw/Assets/Prefabs/GameEnding.cs
@@ -14,7 +14,9 @@ public class GameEnding : MonoBehaviour
     bool m_IsPlayerAtExit;
     bool m_IsPlayerCaught;
     float m_Timer;
-    
+    bool m_HasAudioPlayed;
+
+
     void OnTriggerEnter (Collider other)
     {
         if (other.gameObject == player)
@@ -32,19 +34,28 @@ public class GameEnding : MonoBehaviour
     {
         if (m_IsPlayerAtExit)
         {
-            EndLevel (exitBackgroundImageCanvasGroup, false);
+            EndLevel (exitBackgroundImageCanvasGroup, false, exitAudio);
+             
         }
         else if (m_IsPlayerCaught)
         {
             gameEnding.CaughtPlayer (); 
-            EndLevel (caughtBackgroundImageCanvasGroup, true);
+            EndLevel (caughtBackgroundImageCanvasGroup, true, caughtAudio);
+            
         }
     }
 
-    void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart)
+    void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource); 
     {
+         if (!m_HasAudioPlayed)
+        {
+            audioSource.Play();
+            m_HasAudioPlayed = true;
+        }
+
         m_Timer += Time.deltaTime;
         imageCanvasGroup.alpha = m_Timer / fadeDuration;
+         bool m_HasAudioPlayed; 
 
         if (m_Timer > fadeDuration + displayImageDuration)
         {
